cd198bb24d3f976f1b7011792efdd388ff2a7cd9
chandrakommi <chandrasekhar.kommi@outlook.com>
2023-10-28 14:02:37 +0530

    Configure and Initialise Redux

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,12 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
+
+import { Provider } from 'react-redux'
+import store from './redux/store.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 )
