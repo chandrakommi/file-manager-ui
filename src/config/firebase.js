deb60bb34f126cc076b37d18e682a21ea759c1fa
chandrakommi <136074986+chandrakommi@users.noreply.github.com>
2023-10-28 15:42:34 +0530

    File manager (#1)
    * add prettier rules
    
    * Install and setup Bootstrap@5.3.2
    
    * install redux
    
    * Configure and Initialise Redux
    
    * Install and Initialize firebase
    
    * Add Navigation component
    
    * create a new app in firebase
    
    * Setup firebase hosting

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/compat/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBoE8fGTC3cpvxdubYzmMe4JeDp67X-SMs",
+  authDomain: "react18-file-manager.firebaseapp.com",
+  projectId: "react18-file-manager",
+  storageBucket: "react18-file-manager.appspot.com",
+  messagingSenderId: "596314267378",
+  appId: "1:596314267378:web:2c15eb665612b124fefa6b"
+};
+
+const fire = firebase.initializeApp(firebaseConfig)
---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import { Navigation } from '../../components/HomepageComponents'
+
+const Homepage = () => {
+  return (
+    <>
+      <Navigation />
+      <h1 className='display-1 my-5 text-center '>Welcome to File Manager</h1>
+    </>
+  )
+}
+
+export default Homepage
